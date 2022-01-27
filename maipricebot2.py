9bf20b7fc84a1018780847f1172b49e0754451fa
root <kb@mindsync.ai>
2022-01-27 10:46:09 +0000

    Pancakeswap added

--- maipricebot2.py
+++ maipricebot2.py
@@ -6,8 +6,10 @@ import telebot
 import emoji
 import time
 import os
+from pythonpancakes import PancakeSwapAPI
 
-
+ps = PancakeSwapAPI()
+mai_cake = ps.tokens("0xe985e923b6c52b420dd33549a0ebc2cdeb0ae173")
 w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/d2f78cc8828f4c7b9ac7065a09a6427f"))
 contract = w3.eth.contract(address=Web3.toChecksumAddress("0x068c0626100631db4e0518401e2ab62359bca89b"), abi='''[
     {
@@ -34,7 +36,6 @@ contract = w3.eth.contract(address=Web3.toChecksumAddress("0x068c0626100631db4e0
         "type": "function"
     }
 ]'''
-
 )
 
 
@@ -50,7 +51,13 @@ def get_mai_price():
     bithumb_price = float(json['data'][0]['c'])
     print("MAI price on Bithumb (MAI/USDT):   $%5.4f" % bithumb_price)
 
-    return (bithumb_price, mai_eth_price * eth_price, mai_eth_price, mai_usd_price)
+    ps = PancakeSwapAPI()
+    mai_cake = ps.tokens("0xe985e923b6c52b420dd33549a0ebc2cdeb0ae173")
+    data = mai_cake["data"]
+    mai_bnb_price = float(data["price_BNB"])
+    mai_pcs_usd_price = float(data["price"])
+
+    return (bithumb_price, mai_eth_price * eth_price, mai_eth_price, mai_usd_price, mai_pcs_usd_price, mai_bnb_price)
 
 api_key = os.environ['MINDSYNC_TG_API_KEY']
 bot = telebot.TeleBot(api_key)
@@ -58,20 +65,27 @@ bot = telebot.TeleBot(api_key)
 
 @bot.message_handler(commands=['price'])
 def price(message):
-    (bithumb_usd, univ3_maieth_usd, univ3_eth, univ3_maiusdt) = get_mai_price()
+    (bithumb_usd, univ3_maieth_usd, univ3_eth, univ3_maiusdt, pcs_maiusdt, pcs_maibnb) = get_mai_price()
     bot.reply_to(message, emoji.emojize('''🐳 <b>MAI/USDT: 🚀</b>
 
-<a href="https://www.bithumb.pro/en-us/exchange/professional?q=MAI-USDT">Bithumb</a>: <b>$%.6f</b>
-<a href="https://info.uniswap.org/#/pools/0xf80071b5f13f7ba7504752908ddb449be7e21cc9">Uniswap V3</a>: <b>$%.6f</b>
+$%.6f  <b><a href="https://www.bithumb.pro/en-us/exchange/professional?q=MAI-USDT">Bithumb</a></b>\n
+$%.6f  <b><a href="https://info.uniswap.org/#/pools/0xf80071b5f13f7ba7504752908ddb449be7e21cc9">Uniswap V3</a></b>
 
 🐳 <b>MAI/ETH: 🚀</b>
 
-<a href="https://info.uniswap.org/#/pools/0x8d274f951c4330982146e718a1cee58802b1c372">Uniswap V3</a>: <b>$%.6f</b> = %.9f ETH
+$%.6f  <b><a href="https://info.uniswap.org/#/pools/0x8d274f951c4330982146e718a1cee58802b1c372">Uniswap V3</a></b>
 
-💡 Tip 1: <i>Click on the exchange name to trade MAI</i>\n
-💡 Tip 2: <i>You can buy on one exchange to sell on another (arbitrage)</i>\n
-💡 Tip 3: <i>✅ BUY and HODL </i>🚀🚀🚀
-until Release (12/2021)''' % (bithumb_usd, univ3_maiusdt, univ3_maieth_usd, univ3_eth)), parse_mode='HTML', disable_web_page_preview='true')
+🐳 <b>MAI/BNB: 🚀</b>
+
+$%.6f  <b><a href="https://pancakeswap.finance/swap/0xe985e923b6c52b420dd33549a0ebc2cdeb0ae173">Pancakeswap</a></b>
+
+💡 Tip: <i>Click on the exchange name to trade MAI</i>
+
+🔄 <b>Swap ERC20-BEP20:</b>
+
+https://bridge.mindsync.ai
+\n
+''' % (bithumb_usd, univ3_maiusdt, univ3_maieth_usd, pcs_maiusdt)), parse_mode='HTML', disable_web_page_preview='true')
 
 
 while True:
